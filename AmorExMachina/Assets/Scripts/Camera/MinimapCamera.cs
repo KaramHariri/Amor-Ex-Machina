395ce1041468e7f360453b3428d335b0f922f556
Migge <miggex91@hotmail.com>
2020-04-19 19:45:09 +0200

    Slightly modified the Minimap
    Made it bigger and follows the cameras rotation instead of the players rotation

--- AmorExMachina/Assets/Scripts/Camera/MinimapCamera.cs
+++ AmorExMachina/Assets/Scripts/Camera/MinimapCamera.cs
@@ -6,13 +6,20 @@ public class MinimapCamera : MonoBehaviour
 {
     [SerializeField]
     private PlayerVariables playerVariables;
+    private Camera mainCamera;
 
+    private void Awake()
+    {
+        mainCamera = Camera.main;
+    }
     private void LateUpdate()
     {
         Vector3 newPosition = playerVariables.playerTransform.position;
         newPosition.y = transform.position.y;
         transform.position = newPosition;
 
-        transform.rotation = Quaternion.Euler(90.0f, playerVariables.playerTransform.eulerAngles.y, 0.0f);
+        //transform.rotation = Quaternion.Euler(90.0f, playerVariables.playerTransform.eulerAngles.y, 0.0f);
+        transform.rotation = Quaternion.Euler(90.0f, mainCamera.transform.eulerAngles.y, 0.0f);
+
     }
 }

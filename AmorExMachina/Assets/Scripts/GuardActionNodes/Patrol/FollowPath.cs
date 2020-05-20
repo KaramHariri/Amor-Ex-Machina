b8cfca95841039bf478f7575a29c8502efb075e4
Karam <karam.hariri@outlook.com>
2020-05-20 18:05:04 +0200

    Guard head rotation hotfix.
    the guard's head properly rotates back after looking around.

--- AmorExMachina/Assets/Scripts/GuardActionNodes/Patrol/FollowPath.cs
+++ AmorExMachina/Assets/Scripts/GuardActionNodes/Patrol/FollowPath.cs
@@ -1,4 +1,6 @@
-﻿public class FollowPath : Node
+﻿using UnityEngine;
+
+public class FollowPath : Node
 {
     Guard guard;
 
@@ -12,6 +14,36 @@
         NodeState nodeState = NodeState.FAILURE;
         guard.guardMovement.ResetIdleTimer();
         guard.guardMovement.FollowPath();
+        RotateGuardNeck();
         return nodeState;
+    }
+
+    void RotateGuardNeck()
+    {
+        float guardAngleY = guard.transform.eulerAngles.y;
+        Debug.Log(guardAngleY);
+        if(guardAngleY > 180.0f)
+        {
+            guardAngleY -= 360.0f;
+        }
+        else if(guardAngleY < -180.0f)
+        {
+            guardAngleY += 360.0f;
+        }
+
+        float neckAngleY = guard.guardNeckTransform.localEulerAngles.y;
+        if(neckAngleY > 180.0f)
+        {
+            neckAngleY -= 360.0f;
+        }
+        else if(neckAngleY < -180.0f)
+        {
+            neckAngleY += 360.0f;
+        }
+
+        Quaternion forwardDirection = Quaternion.Euler(0.0f, 0.0f, 0.0f);
+        Quaternion currentNeckRotation = Quaternion.Euler(0.0f, neckAngleY, 0.0f);
+
+        guard.guardNeckTransform.localRotation = Quaternion.Lerp(currentNeckRotation, forwardDirection, Time.realtimeSinceStartup);
     }
 }
